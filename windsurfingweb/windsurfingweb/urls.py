916454e94908027e4ac751abed28f817411a46f2
Takumi <sakai.takumi.54m@st.kyoto-u.ac.jp>
2024-08-31 20:10:20 +1000

    15

--- windsurfingweb/windsurfingweb/urls.py
+++ windsurfingweb/windsurfingweb/urls.py
@@ -87,5 +87,5 @@ urlpatterns = [
     path("api/shushoaisatsu/<int:pk>/", api_views.ShushoaisatsuViewSet.as_view({"get": "retrieve"})),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
