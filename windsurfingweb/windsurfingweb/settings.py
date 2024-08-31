943856bd29ceb43b3564a698087b8f7c01be6c61
Takumi <sakai.takumi.54m@st.kyoto-u.ac.jp>
2024-08-31 17:04:50 +1000

    11

--- windsurfingweb/windsurfingweb/settings.py
+++ windsurfingweb/windsurfingweb/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'windsurfingweb.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE':'django.db.backends.postgresql',
         'NAME': os.environ["PGDATABASE"],
         'USER': os.environ["PGUSER"],
         'PASSWORD': os.environ["PGPASSWORD"],
